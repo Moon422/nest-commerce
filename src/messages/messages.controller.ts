f1e0e0fda63312c67861c10b29dd896746988fbf
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-10-16 09:41:45 +0600

    added class validator and transformer

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Param, Post } from '@nestjs/common'
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  async listMessages() {
+    return { message: 'list messages' }
+  }
+
+  @Post()
+  async createMessage(@Body() body, @Headers() headers) {
+    return { message: 'create message', body, headers }
+  }
+
+  @Get(':id')
+  getMessage(@Param('id') id: number) {
+    return { message: 'get message', id }
+  }
+}
