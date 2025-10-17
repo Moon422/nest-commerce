14bd9c3ee1c610d0b17a20a5724feff53a0ae9c8
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-10-18 00:21:33 +0600

    working with user auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,68 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Session,
+} from '@nestjs/common'
 import { CreateUserDto } from './dtos/create-user.dto'
 import { AuthService } from './auth.service'
 import { LoginUserDto } from './dtos/login-user.dto'
+import { UsersService } from './users.service'
 
 @Controller('auth')
 export class UsersController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+  ) {}
+
+  @Get('colors')
+  async getColor(@Session() session: Record<string, any>) {
+    return session['color']
+  }
+
+  @Get('colors/:color')
+  async setColor(
+    @Param('color') color: string,
+    @Session() session: Record<string, any>,
+  ) {
+    session['color'] = color
+  }
 
   @Post('signup')
-  async createUser(@Body() createUserDto: CreateUserDto) {
+  async createUser(
+    @Body() createUserDto: CreateUserDto,
+    @Session() session: Record<string, any>,
+  ) {
     const { email, password } = createUserDto
-    await this.authService.registerAsync(email, password)
+    const user = await this.authService.registerAsync(email, password)
+
+    session['user-id'] = user.id
+    return { id: user.id, email: user.email }
   }
 
   @Post('signin')
-  async signin(@Body() loginUserDto: LoginUserDto) {
+  async signin(
+    @Body() loginUserDto: LoginUserDto,
+    @Session() session: Record<string, any>,
+  ) {
     const { email, password } = loginUserDto
     const user = await this.authService.verifyAysnc(email, password)
+    if (!user) {
+      return new BadRequestException()
+    }
+
+    session['user-id'] = user.id
+    return { id: user.id, email: user.email }
+  }
+
+  @Get('whoami')
+  async whoAmIAsync(@Session() session: Record<string, any>) {
+    const userId = session['user-id'] as number
+    const user = await this.userService.getUserByIdAsync(userId)
     if (!user) {
       return new BadRequestException()
     }
