fd3bc990469cb76f275e9ac5d37afe0c93b9ac67
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-10-18 22:08:42 +0600

    added tz

--- src/services/customers/customer-password.service.ts
+++ src/services/customers/customer-password.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { genSalt, hash } from 'bcrypt'
+import moment from 'moment'
 import { CustomerPassword } from 'src/entities/customers/customer-password'
 import { Repository } from 'typeorm'
 
@@ -9,4 +11,51 @@ export class CustomerPasswordService {
     @InjectRepository(CustomerPassword)
     private customerPasswordRepository: Repository<CustomerPassword>,
   ) {}
+
+  private async encryptPassword(password: string) {
+    const salt = await genSalt()
+    const encrypted = await hash(password, salt)
+
+    return encrypted
+  }
+
+  async createPasswordAsync(customerId: number, password: string) {
+    const encrypted = await this.encryptPassword(password)
+    const createdOnUtc = moment().utc().toDate()
+    const customerPassword = this.customerPasswordRepository.create({
+      customerId,
+      password: encrypted,
+      createdOnUtc,
+    })
+
+    return await this.customerPasswordRepository.save(customerPassword)
+  }
+
+  async verifyPasswordAsync(customerId: number, password: string) {
+    const customerPasswords = await this.customerPasswordRepository
+      .createQueryBuilder('customerPassword')
+      .orderBy('createdOnUtc', 'DESC', 'NULLS LAST')
+      .getMany()
+
+    if (!customerPasswords || !customerPasswords.length) {
+      return [false, 'Customer do not have passwords.']
+    }
+
+    const encrypted = await this.encryptPassword(password)
+    if (encrypted === customerPasswords[0].password) {
+      return [true, null]
+    }
+
+    for (let i = 1; i < customerPasswords.length; i++) {
+      const customerPassword = customerPasswords[i]
+      if (encrypted === customerPassword.password) {
+        return [
+          false,
+          `Password was changed on ${customerPassword.createdOnUtc}`,
+        ]
+      }
+    }
+
+    return [false, 'Password did not match!']
+  }
 }
