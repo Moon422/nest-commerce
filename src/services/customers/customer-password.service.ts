c57ada5991613c832f4fcf2b69af01fd13d217dd
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2025-10-18 21:42:45 +0600

    added bcrypt

--- src/services/customers/customer-password.service.ts
+++ src/services/customers/customer-password.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { CustomerPassword } from 'src/entities/customers/customer-password'
+import { Repository } from 'typeorm'
 
 @Injectable()
-export class CustomerPasswordService {}
+export class CustomerPasswordService {
+  constructor(
+    @InjectRepository(CustomerPassword)
+    private customerPasswordRepository: Repository<CustomerPassword>,
+  ) {}
+}
